d9d40cf7cc4d8cb3f30ecbb6d75a4554646f6402
piee777 <154609112+piee777@users.noreply.github.com>
2025-10-31 15:52:32 +0100

    feat: Configure Supabase with environment variables
    Integrates Supabase credentials using environment variables for enhanced security and flexibility. Updates build scripts and import maps to support this change. Removes unnecessary Netlify configuration.

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xdukqkzwcqsqwogwyqkh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkdWtxa3p3Y3FzcXdvZ3d5cWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzcwMjEsImV4cCI6MjA3NzM1MzAyMX0.dJxrf84MmWFfULQ-pvLfk5XZkriKrzDLtId8wojrJhs';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 // Export the client directly, but it can be null if credentials are not provided.
 export const supabase = (supabaseUrl && supabaseKey)
